b55a0e593acd7a09b28bb7b203328ef004a9220f
Даниил Косьянов <bazyan99@outlook.com>
2021-05-13 10:32:47 +0300

    Add project files.

---
+++ Chrome_browser/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp.WinForms;
+using CefSharp;
+
+namespace Chrome_browser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ChromiumWebBrowser chrome;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (chrome.CanGoBack)
+                chrome.Back();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CefSettings settings = new CefSettings();
+            Cef.Initialize(settings);
+            textBox1.Text = "https://duckduckgo.com";
+            chrome = new ChromiumWebBrowser(textBox1.Text);
+            this.panel1.Controls.Add(chrome);
+            chrome.Dock = DockStyle.Fill;
+            chrome.AddressChanged += Chrome_AddressChanged;
+        }
+
+        private void Chrome_AddressChanged(object sender, AddressChangedEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(()=> 
+                {
+                textBox1.Text = e.Address;
+                }));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            chrome.Load(textBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            chrome.Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (chrome.CanGoForward)
+                chrome.Forward();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+    }
+}
